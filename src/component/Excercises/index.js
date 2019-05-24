bfe78bf431f80a7556901ff95a248bc8d736d012
Ravi <ravindraafre@gmail.com>
2019-05-24 20:55:10 +0530

    Latest

--- src/component/Excercises/index.js
+++ src/component/Excercises/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import {Grid} from "@material-ui/core"
+import LeftPane from "./LeftPane"
+import RightPane from "./RightPane"
 
-export default props => <h1>Hi from excercise</h1>
+const styles = {
+  Paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10
+  }
+}
+
+export default props => (
+  <Grid container spacing={2}>
+    <Grid item xs>
+      <LeftPane style={styles} />
+    </Grid>
+    <Grid item xs>
+      <RightPane style={styles} />
+    </Grid>
+  </Grid>
+)
--- src/component/Layouts/Footer.js
+++ src/component/Layouts/Footer.js
@@ -1,7 +1,12 @@
 import React from "react"
+import {Paper, Tab, Tabs} from "@material-ui/core"
 
 export default porps => (
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <Paper square>
+    <Tabs value={0} indicatorColor="primary" textColor="primary" centered>
+      <Tab label="Item One" />
+      <Tab label="Item Two" />
+      <Tab label="Item Three" />
+    </Tabs>
+  </Paper>
 )
